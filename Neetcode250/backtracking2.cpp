ecf48a71be30b9dcf800ccf1f9643fe4c6f122a4
Faiz Ahmad <faizahmad43430@gmail.com>
2026-07-09 16:26:57 +0530

    bt

--- Neetcode250/backtracking2.cpp
+++ Neetcode250/backtracking2.cpp
@@ -82,3 +82,85 @@ public:
         return solve(nums, k, visited, 0, target, 0);
     }
 };
+
+// 140. Word Break II
+class Solution {
+public:
+    vector<string> result;
+    unordered_set<string> st;
+    void solve(int i, string &currSentense, string& s){
+        if(i >= s.length()){
+            result.push_back(currSentense);
+            return;
+        }
+        for(int j = i; j < s.length(); ++j){
+            string tempword = s.substr(i, j-i+1);
+            if(st.count(tempword)){ // valid word
+                string tempSentense = currSentense;
+                if(!currSentense.empty()){
+                    currSentense += " ";
+                }
+                currSentense += tempword; // take tempword
+                solve(j+1, currSentense, s);  // Explore 
+                currSentense = tempSentense;
+            }
+        }
+    }
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        for(auto &word: wordDict){
+            st.insert(word);
+        }
+        string currSentense = "";
+        solve(0, currSentense, s);
+        return result;
+    }
+};
+
+// 52. N-Queens II
+class Solution {
+public:
+int N;
+    vector<vector<string>> result;
+
+    bool isValid(vector<string>& board, int row, int col){
+        //Look upward
+        for(int i = row-1; i >= 0; --i){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        //Left diagonal
+        for(int i = row-1, j = col-1; i >= 0 && j>=0; --i, --j){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        //Right Diagonal
+        for(int i = row-1, j = col+1; i>=0 && j>=0 ; --i, ++j){
+            if(board[i][j] == 'Q'){
+                return false;
+            }
+        }
+        return true;
+
+    }
+    void solve(vector<string>& board, int row){
+        if(row >= N){
+            result.push_back(board);
+            return;
+        }
+        for(int col = 0; col < N; col++){
+            if(isValid(board, row, col)){
+                board[row][col] = 'Q';
+                 solve(board, row+1);
+                 board[row][col] = '.';
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        N = n;
+        vector<string> board(N, string(N, '.'));
+        solve(board, 0);
+        return result.size();
+    }
+};
